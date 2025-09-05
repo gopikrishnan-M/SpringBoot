0cafa825a8ef12976989dce92f9e77ff809e65b7
gopi krishnan M <gopikris0021@gmail.com>
2025-09-05 19:36:49 +0530

    Add files via upload

---
+++ SpringOauth2Demo/src/main/java/com/kris/SpringOauth2Demo/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.kris.SpringOauth2Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "welcome to Kris OauthSecurity";
+    }
+}
